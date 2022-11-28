39db012bd18fed62ca4514bad68cd0efa68ac4a5
Nayem Jaman Tusher <njaman26@gmail.com>
2022-11-28 15:17:04 +0600

    logout

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('all_books', views.all_books, name= 'all_books'),
-    path('category_detail/<str:slug>', views.category_detail, name= 'category_detail'),
-    path('book_detail/<str:slug>', views.book_detail, name= 'book_detail'),
-    path('search_book', views.search_book, name= 'search_book'),
-    path('register', views.register, name= 'register'),
-    path('user_login', views.user_login, name= 'user_login'),
-    path('logout', views.logout, name= 'logout'),
+    path('all_books', views.all_books, name='all_books'),
+    path('category_detail/<str:slug>',
+         views.category_detail, name='category_detail'),
+    path('book_detail/<str:slug>', views.book_detail, name='book_detail'),
+    path('search_book', views.search_book, name='search_book'),
+    path('register', views.register, name='register'),
+    path('user_login', views.user_login, name='user_login'),
+    path('user_logout', views.user_logout, name='user_logout'),
 
 ]
--- bookapp/views.py
+++ bookapp/views.py
@@ -1,51 +1,59 @@
-from django.shortcuts import render, HttpResponse,redirect
+from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 # core = booksite
 def index(request):
-    recommended_books = Book.objects.filter(recommended_books= True)
-    fiction_books = Book.objects.filter(fiction_books= True)
-    business_books = Book.objects.filter(business_books= True)
+    recommended_books = Book.objects.filter(recommended_books=True)
+    fiction_books = Book.objects.filter(fiction_books=True)
+    business_books = Book.objects.filter(business_books=True)
     context = {
-        'recommended_books':recommended_books,
-        'fiction_books':fiction_books,
-        'business_books':business_books
+        'recommended_books': recommended_books,
+        'fiction_books': fiction_books,
+        'business_books': business_books
     }
     return render(request, 'index.html', context)
 
+
 def all_books(request):
-    books =Book.objects.all()
+    books = Book.objects.all()
     context = {
         'books': books
     }
-    return render(request,'all_books.html', context)
+    return render(request, 'all_books.html', context)
 
 
 def category_detail(request, slug):
     category = Category.objects.get(slug=slug)
-    context ={
+    context = {
         'category': category
     }
     return render(request, 'category_detail.html', context)
 
-def book_detail(request,slug):
+
+@login_required(login_url='user_login')
+def book_detail(request, slug):
     book = Book.objects.get(slug=slug)
     books_category = book.category.first()
-    similar_books = Book.objects.filter(category__name__startswith=books_category)
+    similar_books = Book.objects.filter(
+        category__name__startswith=books_category)
     context = {
         'book': book,
         'similar_books': similar_books,
-        'books_category':books_category
+        'books_category': books_category
     }
-    return render(request, 'book_detail.html',context)
+    return render(request, 'book_detail.html', context)
 
 
 def search_book(request):
-    search_books =  Book.objects.filter(title__icontains=request.POST.get('name_of_book'))
-    return render(request,'search_book.html',{'search_books':search_books})
+    search_books = Book.objects.filter(
+        title__icontains=request.POST.get('name_of_book'))
+    return render(request, 'search_book.html', {'search_books': search_books})
 
 
 def register(request):
@@ -55,24 +63,25 @@ def register(request):
         if register_form.is_valid():
             register_form.save()
             messages.info(request, "Account Created Successfully!")
-            return redirect('login')
-            
-    context = {'register_form':register_form}
-    return render(request,'register.html',context)
+            return redirect('user_login')
 
-    
+    context = {'register_form': register_form}
+    return render(request, 'register.html', context)
+
+
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password1')
-        user = authenticate(request, username = username, password = password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('index')
         else:
-            messages.info(request, "Invalid User")    
-    return render(request, 'login.html')
+            messages.info(request, "Invalid User")
+    return render(request, 'user_login.html')
 
 
-def logout(request):
-    return render(request,'logout.html')
+def user_logout(request):
+    logout(request)
+    return redirect('index')
