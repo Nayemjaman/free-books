5070ef0c3b5da4f5f48a32e216b69bc7e02de569
Nayem Jaman Tusher <njaman26@gmail.com>
2023-01-07 17:16:32 +0600

    fix UI

--- bookapp/forms.py
+++ bookapp/forms.py
@@ -9,6 +9,10 @@ class BookSearchForm(forms.ModelForm):
     name_of_book = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
         'class':"form-control rounded", 'placeholder':"Enter book name"
     }))
+    name_of_book = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'class':"form-control mr-sm-2", 'placeholder':"Enter book name"
+
+    }))
     class Meta:
         model = BookSearch
         fields = ['name_of_book',]
