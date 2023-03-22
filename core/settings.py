135e3f9a2eb7439e5384d8465e01c2f6bccbeb4f
Nayem Jaman Tusher <40755491+Nayemjaman@users.noreply.github.com>
2023-03-22 23:26:15 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -134,5 +134,5 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
