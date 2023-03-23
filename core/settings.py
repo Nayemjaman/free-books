5c19c83f5251991c3c0e2cb04f1723e97edb31ef
Nayem Jaman Tusher <njaman26@gmail.com>
2023-03-24 01:59:20 +0600

    deployment 0.1

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-$m_-7(9=^o(#c&=en5d0qta#(-zf$90n)uhy7h(!3&p%z(8$g3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["free-books.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 # ALLOWED_HOSTS = []
 
 CUSTOM_APPS = [
